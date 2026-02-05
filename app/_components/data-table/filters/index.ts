522a10e632c182f1b37f6549db853bbddc2d2592
Vunky Himawan <tingkerbeii44@gmail.com>
2026-02-05 21:16:47 +0700

    feat: add DateRangePicker to filters and clean up calendar component code

--- app/_components/data-table/filters/index.ts
+++ app/_components/data-table/filters/index.ts
@@ -1,6 +1,8 @@
+import { DateRangePicker } from "./date-range-picker";
 import { SelectFilter } from "./select";
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
   SelectFilter,
+  DateRangePicker,
 };
